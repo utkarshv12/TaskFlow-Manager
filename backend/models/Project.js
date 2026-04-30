e1dbcfaedfbe6e82f643c6db347d3d0666ebc1f2
Utkarsh <vermapriyank730@gmail.com>
2026-05-01 05:24:45 +0530

    Add files via upload

---
+++ backend/models/Project.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
